c2eb234a4aaea953ed049d2cf0d4f2878a855a6d
yanmyoaung2004 <ymyo44277@gmail.com>
2024-04-15 22:13:05 +0630

    create the header section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,10 +5,12 @@ import Signup from "./pages/Signup";
 import About from "./pages/About";
 import Dashboard from "./pages/Dashboard";
 import Projects from "./pages/Projects";
+import Header from "./components/Header";
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/sign-in" element={<Signin />} />
--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -1,3 +1,8 @@
+import { Button } from "flowbite-react";
 export default function Signin() {
-  return <div>Signin</div>;
+  return (
+    <div>
+      <Button>Click me</Button>;
+    </div>
+  );
 }
