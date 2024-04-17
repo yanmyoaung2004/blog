5c3971ea2ed904da64823bf0fb607162c39a51f9
yanmyoaung2004 <ymyo44277@gmail.com>
2024-04-17 15:36:35 +0630

    create a post api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -49,6 +49,7 @@ export const signin = async (req, res, next) => {
     const token = jwt.sign(
       {
         id: validUser._id,
+        isAdmin: validUser.isAdmin,
       },
       process.env.JWT_SECRET
     );
@@ -69,7 +70,10 @@ export const google = async (req, res) => {
   try {
     const user = await User.findOne({ email: email });
     if (user) {
-      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+      const token = jwt.sign(
+        { id: user._id, isAdmin: user.isAdmin },
+        process.env.JWT_SECRET
+      );
       const { password, ...rest } = user._doc;
       res
         .status(200)
@@ -90,7 +94,10 @@ export const google = async (req, res) => {
         profilePicture: googlePhotoUrl,
       });
       await newUser.save();
-      const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET);
+      const token = jwt.sign(
+        { id: newUser._id, isAdmin: newUser.isAdmin },
+        process.env.JWT_SECRET
+      );
       const { password, ...rest } = newUser._doc;
       res
         .status(200)
